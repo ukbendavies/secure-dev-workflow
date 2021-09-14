7f0b857a6d3526a85f6f22fab36f8c0970af900e
Ben Davies <ukbendavies@yahoo.com>
2021-09-14 13:40:52 +0100

    add baseline pwsh profile

---
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# powershell profile
+#
+# features
+#  - powershell git extensions
+#  - theme for git
+#
+# (c) ben davies
+
+# profile extension for git
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
