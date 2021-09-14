9c1de6cddc28f15432d4794d84187bb623431f4c
Ben Davies <ben.davies@citrix.com>
2021-09-14 14:51:36 +0100

    support ssh-agent using openssh on windows 10

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,14 @@
 # powershell profile
 #
 # features
+#  - support ssh-agent using openssh on windows 10
 #  - powershell git extensions
 #  - theme for git
 #
 # (c) ben davies
+
+# use openssh on windows 10 (requires openssh optional component)
+$Env:GIT_SSH=$((Get-Command -Name ssh).Source)
 
 # extension to enhnce the prompt for git
 # Requires: om-my-posh v3 and the following imports
