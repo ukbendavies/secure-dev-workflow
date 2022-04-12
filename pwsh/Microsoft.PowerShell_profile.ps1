dd6d202d188773afe8d6a2b0d37f742f002c1369
Ben Davies <ukbendavies@yahoo.com>
2022-04-12 21:23:46 +0100

    clarify comments, update to new cmdlet Set-PoshPrompt; required change as of oh-my-posh as of 2.0.492+

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -3,15 +3,17 @@
 # features
 #  - support ssh-agent for password protected keys using openssh on windows 10 / 11
 #  - helper functions for efficient source code directory traversal
-#  - theme for git requires oh-my-posh and font support see readme.md
-#  - posh git extensions
+#  - powershell git prompt and theme, oh-my-posh, required: 2.0.492+
+#    and optionally unicode font support, see terminal/readme.md
+#    oh-my-posh, required: 2.0.492+
+#  - posh-git, required: 0.7.3+
 #
 # (c) ben davies
 
 # environment
 $promptTheme = 'paradox'                          # oh my posh theme choice
 $codeDir = (Resolve-Path '~/src').Path            # your source code path, example is: "C:\Users\[your username]\src"
-$env:path += ";$($env:SystemDrive)\local\bin"     # your approved custom local tools on the path here
+$env:path += ";$($env:SystemDrive)\local\bin"     # your approved custom local tools to include on the path here
 $env:GIT_SSH = $((Get-Command -Name ssh).Source)  # use windows openssh ssh-agent
 
 # custom commands
@@ -43,4 +45,4 @@ Register-ArgumentCompleter -CommandName cdc -ParameterName Folder -ScriptBlock {
 # custom shell prompts
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme $promptTheme
+Set-Theme $promptTheme
