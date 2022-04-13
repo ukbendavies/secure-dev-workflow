9ced53e15c7967fc2075d9f551dcc95212105a0a
Ben Davies <ukbendavies@yahoo.com>
2022-04-13 02:39:25 +0100

    improve variable naming and improve powershell help and examples for cdc

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -23,31 +23,40 @@ $predictionSource = 'History'                     # All commands History
 function autocompletepath {
   param (
     [String] $Path,
-    [String] $WordToComplete
+    [String] $Pattern
   )
   return Get-ChildItem -Path $Path -Recurse -Depth 2 |
   Select-Object -ExpandProperty FullName |
   ForEach-Object { $_ -replace "$($Path -replace "\\", "\\")\\" } |
   ForEach-Object { $_ -replace "\\", "/" } |
-  Where-Object { $_ -like "${WordToComplete}*" } |
+  Where-Object { $_ -like "${Pattern}*" } |
   ForEach-Object { "$_/" }
 }
 
 function cdc {
   #.SYNOPSIS
-  # Start typing to auto complete directory in SourcePath.
+  # Start typing to auto complete a directory in SourcePath when TAB is pressed.
   # Tab to rotate through the list or fully complete a discovered directory name.
-  # Changes to that directory when enter is pressed.
+  # Enter changes directory to the current selection.
+  #.EXAMPLE
+  #   cdc
+  #   - changes to your preconfigured SourcePath
+  #.EXAMPLE
+  #   cdc github/*-dev
+  #   - press TAB > autocomplete algorithm finds 'github/secure-dev-workflow'
+  #.EXAMPLE
+  #   cdc g*/*/pw*
+  #   - press TAB > autocomplete algorithm finds 'github/secure-dev-workflow/pwsh'
   param (
-    # Directory to autocomplete in SourcePath
+    # Directory to autocomplete within SourcePath
     [String] $Folder
   )
   Set-Location "${SourcePath}/${Folder}/"
 }
 
 Register-ArgumentCompleter -CommandName cdc -ParameterName Folder -ScriptBlock {
-  param ($CommandName, $ParameterName, $WordToComplete)
-  autocompletepath -Path $SourcePath -WordToComplete $WordToComplete
+  param ($CommandName, $ParameterName, $Pattern)
+  autocompletepath -Path $SourcePath -Pattern $Pattern
 }
 
 # Enabled Features
